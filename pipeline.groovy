2865d775aabb12af3b1ec9f7961c8b896e854878
JayLikhare316 <likharjee1@gmail.com>
2025-04-28 17:50:29 +0530

    .

--- pipeline.groovy
+++ pipeline.groovy
@@ -9,11 +9,12 @@ pipeline {
         stage('code-build') {
             steps {
                 sh '''
-                    npm install
-                    ng build --configuration production
+                sudo chown -R jenkins:jenkins /var/lib/jenkins/workspace/frontend-deployment
+                npm install
+                ng build --configuration production
                 '''
             }
-        }
+}
         stage('code-deploy') {
             steps {
                 withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
