6b5cd6e4c84beda6af81f95332a109c45e725241
JayLikhare316 <likharjee1@gmail.com>
2025-04-28 17:47:52 +0530

    .

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,27 +1,26 @@
 pipeline {
     agent any 
-    stages{
-        stage('code-pull'){
+    stages {
+        stage('code-pull') {
             steps {
                 git branch: 'main', url: 'https://github.com/JayLikhare316/project-frontend.git'
             }
         }
-        stage('code-build'){
+        stage('code-build') {
             steps {
                 sh '''
                     npm install
-                    ng build
+                    ng build --configuration production
                 '''
             }
         }
-        stage('code-deploy'){
+        stage('code-deploy') {
             steps {
                 withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-                  sh '''
-                      aws s3 cp --recursive dist/angular-frontend s3://jaybhaukifrontends3bucket/
-                 '''
-}
-                
+                    sh '''
+                        aws s3 cp --recursive dist/angular-frontend s3://jaybhaukifrontends3bucket/
+                    '''
+                }
             }
         }
     }
